2585616b48f587d84208f6ff8d44f6e2eb5b0fd9
nkj36 <nkj36@naver.com>
2018-11-12 05:05:16 +0900

    Main_Home Second Test

--- src/choice_helper/Main_home.java
+++ src/choice_helper/Main_home.java
@@ -4,7 +4,7 @@ public class Main_home {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("메인화면 테스트 입니다.");
+		System.out.println("Main test!");
 	}
 
 }
