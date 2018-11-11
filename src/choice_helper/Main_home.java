343d0f338988380c6bcdaaf168482e5f74f10b89
nkj36 <nkj36@naver.com>
2018-11-12 04:29:59 +0900

    Main_home frame First Commit

---
+++ src/choice_helper/Main_home.java
@@ -0,0 +1,10 @@
+package choice_helper;
+
+public class Main_home {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("메인화면 테스트 입니다.");
+	}
+
+}
