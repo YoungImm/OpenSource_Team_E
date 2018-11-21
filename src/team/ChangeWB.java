9531ce62c509e17677cefcbd6c6e8da47f1caa24
dnfwlxo11 <dnfwlxo11@naver.com>
2018-11-21 13:29:57 +0900

    글꼴 변경

--- src/team/ChangeWB.java
+++ src/team/ChangeWB.java
@@ -65,20 +65,20 @@ public class ChangeWB extends JFrame {
 		JButton btnNewButton = new JButton("\uC2DC\uC791");
 		btnNewButton.setBackground(new Color(230, 230, 250));
 		btnNewButton.setForeground(new Color(106, 90, 205));
-		btnNewButton.setFont(new Font("배달의민족 한나는 열한살", Font.PLAIN, 27));
+		btnNewButton.setFont(new Font("배달의민족 도현", Font.PLAIN, 27));
 		btnNewButton.setBounds(90, 368, 179, 36);
 		contentPane.add(btnNewButton);
 		
 		JButton button = new JButton("\uC815\uC9C0");
 		button.setBackground(new Color(255, 204, 255));
 		button.setForeground(new Color(186, 85, 211));
-		button.setFont(new Font("배달의민족 한나는 열한살", Font.PLAIN, 27));
+		button.setFont(new Font("배달의민족 도현", Font.PLAIN, 27));
 		button.setBounds(318, 368, 179, 36);
 		contentPane.add(button);
 		
 		JLabel lblNewLabel = new JLabel("");
-		lblNewLabel.setIcon(new ImageIcon("C:\\Users\\\uB85C\uC9C11\\Downloads\\party (3).png"));
-		lblNewLabel.setBounds(173, 80, 274, 256);
+		lblNewLabel.setIcon(new ImageIcon("C:\\Users\\chosun\\Downloads\\videogame (1).png"));
+		lblNewLabel.setBounds(104, 192, 76, 61);
 		contentPane.add(lblNewLabel);
 		
 		JLabel label = new JLabel("\uB180\uAC70\uB9AC");
@@ -90,7 +90,7 @@ public class ChangeWB extends JFrame {
 		
 		JButton btnNewButton_1 = new JButton("");
 		btnNewButton_1.setBackground(new Color(186, 85, 211));
-		btnNewButton_1.setIcon(new ImageIcon("C:\\Users\\\uB85C\uC9C11\\Downloads\\return-button (2).png"));
+		btnNewButton_1.setIcon(new ImageIcon("C:\\Users\\chosun\\Downloads\\return-button.png"));
 		btnNewButton_1.setBounds(24, 40, 49, 47);
 		contentPane.add(btnNewButton_1);
 		
@@ -109,9 +109,29 @@ public class ChangeWB extends JFrame {
 		contentPane.add(lblNewLabel_1);
 		
 		lblNewLabel_2 = new JLabel("");
-		lblNewLabel_2.setIcon(new ImageIcon("C:\\Users\\\uB85C\uC9C11\\Downloads\\musical-note.png"));
-		lblNewLabel_2.setBounds(397, 111, 41, 61);
+		lblNewLabel_2.setIcon(new ImageIcon("C:\\Users\\chosun\\Downloads\\clapperboard (1).png"));
+		lblNewLabel_2.setBounds(168, 114, 69, 61);
 		contentPane.add(lblNewLabel_2);
+		
+		JLabel lblNewLabel_3 = new JLabel("");
+		lblNewLabel_3.setIcon(new ImageIcon("C:\\Users\\chosun\\Downloads\\football.png"));
+		lblNewLabel_3.setBounds(270, 92, 81, 71);
+		contentPane.add(lblNewLabel_3);
+		
+		JLabel lblNewLabel_4 = new JLabel("");
+		lblNewLabel_4.setIcon(new ImageIcon("C:\\Users\\chosun\\Downloads\\microphone.png"));
+		lblNewLabel_4.setBounds(363, 114, 81, 71);
+		contentPane.add(lblNewLabel_4);
+		
+		JLabel lblNewLabel_5 = new JLabel("");
+		lblNewLabel_5.setIcon(new ImageIcon("C:\\Users\\chosun\\Downloads\\cards.png"));
+		lblNewLabel_5.setBounds(413, 192, 81, 71);
+		contentPane.add(lblNewLabel_5);
+		
+		JLabel lblNewLabel_6 = new JLabel("");
+		lblNewLabel_6.setIcon(new ImageIcon("C:\\Users\\chosun\\Downloads\\question.png"));
+		lblNewLabel_6.setBounds(233, 162, 145, 163);
+		contentPane.add(lblNewLabel_6);
 		
 		setUndecorated(true);
 	
